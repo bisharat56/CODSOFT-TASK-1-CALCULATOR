dcbc01856cd359ffce7b987113114a387827fc84
Bisharat56 <engineer.bisharatali@gmail.com>
2023-08-08 00:36:47 +0500

    Calculator-App-Task

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 input_txt.setText("");
                 result_txt.setText("");
-                result_txt.setText("");
+
             }
         });
 
